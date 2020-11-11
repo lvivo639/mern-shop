c40b33ccbf830c25c10ca07c03c346d88e1027e7
volmk <lvi.work1@gmail.com>
2020-11-11 15:27:31 +0200

    bugfix

--- client/src/components/Paginate.js
+++ client/src/components/Paginate.js
@@ -8,12 +8,11 @@ const Paginate = ({pages, page, toLink}) => {
                 <ul className="pagination">
                     {[...Array(pages).keys()].map((x) =>
                         <li className={`page-item ${x + 1 === page ? 'active' : ''}`}>
-                            <span className="page-link">
-                                <Link key={x + 1}
-                                      to={`${toLink}${x + 1}`}>
+                            <Link key={x + 1} to={`${toLink}${x + 1}`}>
+                                <span className="page-link">
                                     {x + 1}
-                                </Link>
-                            </span>
+                                </span>
+                            </Link>
                         </li>
                     )}
                 </ul>
